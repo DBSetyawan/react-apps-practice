20e9379a197192f8f3e63640efba9a7fae1d7d20
Daniel <artexsdns@gmail.com>
2020-02-17 12:02:08 +0700

    enabled cors & upgrade package

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './index.css'
 import Table from './table.js';
 import Form from './form/form.js';
+import APIdevelopment from './API/api-dev.js';
 
 class App extends Component {
   
@@ -45,6 +46,9 @@ class App extends Component {
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
         <Form handleSubmit={this.handleSubmit}/>
+        <br/>
+        <p> Mengambil API dan menambahkan state component :</p>
+        <APIdevelopment />
       </div>
     )
   }
